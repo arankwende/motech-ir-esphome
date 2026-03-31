8ebac05ed78033bf3146679d74ebc2b91ba0bc9a
Carlos Alfredo Diaz <cdiazjr4@gmail.com>
2026-03-31 00:48:19 -0300

    Update climate.py

--- components/motech_ac/climate.py
+++ components/motech_ac/climate.py
@@ -1,9 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, remote_transmitter
-from esphome.const import (
-    CONF_ID,
-)
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["climate", "remote_transmitter"]
 
@@ -14,18 +12,16 @@ MotechACClimate = motech_ac_ns.class_(
 
 CONF_TRANSMITTER_ID = "transmitter_id"
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+CONFIG_SCHEMA = climate.climate_schema(MotechACClimate).extend(
     {
-        cv.GenerateID(): cv.declare_id(MotechACClimate),
         cv.Required(CONF_TRANSMITTER_ID): cv.use_id(
             remote_transmitter.RemoteTransmitterComponent
         ),
     }
-).extend(cv.COMPONENT_SCHEMA)
+)
 
 
 async def to_code(config):
-    cg.add_global(cg.RawExpression('#include "motech_ac_climate.h"'))
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
